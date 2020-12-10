c2501de741cee4aa3294cad26371261ecb234dac
Julia Chancey <julia@Julias-MacBook-Air.local>
2020-12-10 16:07:06 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/HashTree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HashTree.dir/main.cpp.o"
+  "HashTree"
+  "HashTree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HashTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
